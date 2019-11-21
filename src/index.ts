2881fab1e7c652263c40feaeac3428dc111505e5
Denis Hamann <dh@taktik.com>
2019-11-21 13:42:35 +0100

    feat(mac): added same output format

--- src/index.ts
+++ src/index.ts
@@ -44,61 +44,6 @@ class NetworkEverywhereImpl implements NetworkEverywhere {
 		this.command = this.osType === OSType.Windows ? '%SystemRoot%/System32/getmac.exe' : '/sbin/ifconfig -a || /sbin/ip link'
 	}
 
-	async getActiveInterface(): Promise<{ ip: string, mac: string }> {
-		const activeIface = await this.getActiveInterfaceName()
-		const networkInterfaces = os.networkInterfaces()
-		if (networkInterfaces[activeIface]) {
-			const iface = networkInterfaces[activeIface].find(iface => iface.family === 'IPv4')
-			if (iface) {
-				return { ip: iface.address, mac: iface.mac }
-			}
-		}
-		throw new Error('No suitable active interface found')
-	}
-
-	async getIpAddress(): Promise<string> {
-		const { ip } = await this.getActiveInterface()
-		if (ip) {
-			return ip
-		}
-		const candidate = _.flatMap(_.values(os.networkInterfaces())).find((iface) => iface.family === 'IPv4' && !iface.internal)
-		if (candidate && candidate.address) {
-			return candidate.address
-		}
-		throw new Error('No suitable ip address found')
-	}
-
-	async getAllMacAddresses(): Promise<string[]> {
-		const { stdout } = await execAsync(this.command)
-		return NetworkEverywhereImpl.pullMac(stdout)
-	}
-
-	private async getActiveInterfaceName(): Promise<string> {
-
-		if (this.osType === OSType.Windows) {
-			return new Promise((resolve, reject) => {
-				wmic.get_value('nic', 'NetConnectionID', 'NetConnectionStatus = 2', (err: any, value: string) => {
-					if (err) {
-						reject(err)
-					}
-					resolve(value)
-				})
-			})
-		} else {
-			const cmd = `netstat -rn | grep UG | awk '{print $NF}'`
-
-			const { stderr, stdout } = await execAsync(cmd)
-			if (stderr) {
-				throw new Error('Fail to retrieve active interface')
-			}
-			const raw = stdout.toString().trim().split('\n')
-			if (raw.length === 0 || raw === ['']) {
-				throw new Error('No active network interface found.')
-			}
-			return raw[0]
-		}
-	}
-
 	private static pullMac(data: string, iface?: string): string[] {
 		const toExtract = iface ? NetworkEverywhereImpl.filterByInterface(iface, data) : data
 		const result = []
@@ -130,6 +75,65 @@ class NetworkEverywhereImpl implements NetworkEverywhere {
 			}
 		}
 		return result
+	}
+
+	async getActiveInterface(): Promise<{ ip: string, mac: string }> {
+		const activeIface = await this.getActiveInterfaceName()
+		const networkInterfaces = os.networkInterfaces()
+		if (networkInterfaces[activeIface]) {
+			const iface = networkInterfaces[activeIface].find(iface => iface.family === 'IPv4')
+			if (iface) {
+				return { ip: iface.address, mac: this.formatMacAddress(iface.mac) }
+			}
+		}
+		throw new Error('No suitable active interface found')
+	}
+
+	async getIpAddress(): Promise<string> {
+		const { ip } = await this.getActiveInterface()
+		if (ip) {
+			return ip
+		}
+		const candidate = _.flatMap(_.values(os.networkInterfaces())).find((iface) => iface.family === 'IPv4' && !iface.internal)
+		if (candidate && candidate.address) {
+			return candidate.address
+		}
+		throw new Error('No suitable ip address found')
+	}
+
+	async getAllMacAddresses(): Promise<string[]> {
+		const { stdout } = await execAsync(this.command)
+		return NetworkEverywhereImpl.pullMac(stdout).map(originalMac => this.formatMacAddress(originalMac))
+	}
+
+	private formatMacAddress(originalMac: string): string {
+		return originalMac.toLocaleLowerCase().replace(/-/g, ':')
+	}
+
+	private async getActiveInterfaceName(): Promise<string> {
+
+		if (this.osType === OSType.Windows) {
+			return new Promise((resolve, reject) => {
+				wmic.get_value('nic', 'NetConnectionID', 'NetConnectionStatus = 2', (err: any, value: string) => {
+					if (err) {
+						reject(err)
+					}
+					resolve(value)
+				})
+			})
+		} else {
+			const cmd = `netstat -rn | grep UG | awk '{print $NF}'`
+
+			const { stderr, stdout } = await execAsync(cmd)
+			if (stderr) {
+				throw new Error('Fail to retrieve active interface')
+			}
+			const raw = stdout.toString().trim().split('\n')
+			if (raw.length === 0 || raw === ['']) {
+				throw new Error('No active network interface found.')
+			}
+			return raw[0]
+		}
 	}
 
 }
